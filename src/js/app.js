1d94fc343eaa9ec582cc46f034474d4d6910966b
Luis Lobo <luis.lobo9b@gmail.com>
2024-11-10 04:48:24 -0300

    importado em app.js o store e o slice, criado a função getCounter

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
+import store from "./store/store.js"
+import { decrementCounter, incrementCounter, setCounter } from "./store/slices/counterSlice.js"
+
+function getCounter() {
+	return store.getState().counter.value
+}
+
 console.log("RTK", RTK)
+console.log("store", store)
