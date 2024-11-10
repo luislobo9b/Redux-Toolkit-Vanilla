f4c9712c0994b9d1317de0f38a0fe3e43f33013c
Luis Lobo <luis.lobo9b@gmail.com>
2024-11-10 04:50:48 -0300

    atualizado app.js com todos os botões e eventos para cada um deles usando os dispatchers correspondentes, inscrito no store para receber a atualização e criado a função render para sincronizar a atualização com o DOM

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,36 @@ function getCounter() {
 
 console.log("RTK", RTK)
 console.log("store", store)
+
+const btnGetCounter = document.querySelector("#getCounter"),
+	btnSetCounter = document.querySelector("#setCounter"),
+	inputCounter = document.querySelector("#inputCounter"),
+	btnDecrementCounter = document.querySelector("#decrementCounter"),
+	btnIncrementCounter = document.querySelector("#incrementCounter"),
+	resultElement = document.querySelector("#result")
+
+btnGetCounter.addEventListener("click", () => {
+	const counter = getCounter()
+	console.log(counter)
+})
+
+btnSetCounter.addEventListener("click", () => {
+	const value = Number(inputCounter.value)
+	inputCounter.value = 0
+	store.dispatch(setCounter(value))
+})
+
+btnDecrementCounter.addEventListener("click", () => {
+	store.dispatch(decrementCounter())
+})
+
+btnIncrementCounter.addEventListener("click", () => {
+	store.dispatch(incrementCounter())
+})
+
+store.subscribe(render)
+
+function render() {
+	const counter = getCounter()
+	resultElement.textContent = counter.toString()
+}
